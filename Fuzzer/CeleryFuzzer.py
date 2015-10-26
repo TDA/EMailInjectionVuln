1151f160dc5a1e74a6f9b8eaaa6f30c9a9c58b54
Sai Pc <schand31@asu.edu>
2015-10-25 20:04:17 -0700

    moaar fuzz basics

--- Fuzzer/CeleryFuzzer.py
+++ Fuzzer/CeleryFuzzer.py
@@ -1,1 +1,18 @@
+from __future__ import absolute_import
+
+from celery import Celery
+
 __author__ = 'saipc'
+
+app = Celery('CeleryFuzzer',
+             broker='amqp://',
+             backend='amqp://',
+             include=['Fuzzer.email_form_retriever'])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+)
+
+if __name__ == '__main__':
+    app.start()
--- Fuzzer/email_form_retriever.py
+++ Fuzzer/email_form_retriever.py
@@ -1,1 +1,28 @@
+from __future__ import absolute_import
 __author__ = 'saipc'
+from bs4 import BeautifulSoup
+import re
+import mysql.connector
+from celery import Celery
+
+from Crawler.functions import *
+from Fuzzer.CeleryFuzzer import app
+
+@app.task(name='Fuzzer.email_form_retriever')
+def email_form_retriever():
+    try:
+        db = getopenconnection()
+        cursor = db.cursor()
+        # Lets get all the email forms from the db and print them for now
+        TABLE_NAME = 'email_forms'
+        search_query = generate_search_query(TABLE_NAME, 'id, form_id', 'form_id', 1)
+        print(search_query)
+        # cursor.execute(search_query)
+        db.commit()
+
+    except Exception as e:
+        print("Definitely a database issue, well, hopefully.")
+        print(e)
+        return
+
+
