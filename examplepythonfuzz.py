2ad5e5dff82b4906c931f7079477c36961a8a385
Sai Pc <schand31@asu.edu>
2015-12-25 22:29:52 +0530

    eg pgm for python email fuzzing

---
+++ examplepythonfuzz.py
@@ -0,0 +1,25 @@
+__author__ = 'saipc'
+
+from email.parser import Parser
+
+to = input()
+#print to
+msg = 'To: '+ to + '\n' \
+        'From: <user@example.com>\n' \
+        'Subject: Test message\n' \
+        '\n'\
+        'Body would go here\n'
+
+#print msg
+headers = Parser().parsestr(msg)
+
+#  Now the header items can be accessed as a dictionary:
+# attack string => 'sai@sai.com\nBCC:spc\nbcc:saipc'
+# Parser().parsestr checks for the BCC/bcc field, and
+# only takes in the FIRST found field
+# this behavior is opposite to that of
+# the PHP mail() function :O
+print 'To: %s' % headers['to']
+print 'From: %s' % headers['from']
+print 'Subject: %s' % headers['subject']
+print 'BCC: %s' % headers['bcc']
