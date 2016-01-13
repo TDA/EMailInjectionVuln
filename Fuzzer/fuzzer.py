029340e15d2fcee22f00a46210a0a7e8cee2b421
Sai Pc <schand31@asu.edu>
2016-01-13 23:53:52 +0530

    figuring out how to form requests

--- Fuzzer/fuzzer.py
+++ Fuzzer/fuzzer.py
@@ -6,11 +6,6 @@ from urllib.parse import *
 
 def fuzzer(reconstructed_form):
     try:
-        # TODO have to get the individual attrs and inputs
-        # from the form, and make sure that the fuzzing is only
-        # done on the email fields, also, set the GET/POST right
-        # and check with localmail first before whambamming
-
         # lets print out everything we have so we know what all we have :O
         # print(reconstructed_form)
         main_url, attributes, method, action, input_list = reconstructed_form
@@ -19,8 +14,8 @@ def fuzzer(reconstructed_form):
         print("Method:", method)
         print("Action:", action)
         print("Input list:", input_list)
-        main_url = 'http://hiya.com/signup/sss/pcs'
-        action = 'http://xyz.com/submit.php'
+        main_url = 'http://localhost:63343/htdocs/TestProject/email.htm'
+        action = 'http://localhost:63343/htdocs/TestProject/MailTest.php'
 
         # only do the following IFF the action is relative, and NOT absolute
         # (ie) if /submit.php and NOT http://xyz.com/submit.php, since in the latter
@@ -47,22 +42,30 @@ def fuzzer(reconstructed_form):
 
         print("Final URL:", url)
 
+
+        # TODO have to get the individual attrs and inputs
+        # from the form, and make sure that the fuzzing is only
+        # done on the email fields, also, set the GET/POST right
+        # and check with localmail first before whambamming
+
+        # now reconstruct the form
+        data = None
+
         method = str(method).lower()
         # Since we are using requests (yay!), we dont have
         # to construct the url as per get or post
         if method == 'get':
             # make a get request with requests
+            # r = requests.get(url, data)
             pass
         elif method == 'post':
             # make a post request with requests
             pass
         else:
-            # we dont have to do this, handle only gets
+            # we dont have to do this, we handle only gets
             # or posts, no need to complicate
             return
 
-        # now reconstruct the form
-        data = None
 
         # r = requests.post(url, data)
 
