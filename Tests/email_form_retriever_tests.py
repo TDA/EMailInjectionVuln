c2037de09260fdfa140370c9e2953745a91cc48d
Sai Pc <schand31@asu.edu>
2016-01-20 16:39:19 -0700

    first test written

--- Tests/email_form_retriever_tests.py
+++ Tests/email_form_retriever_tests.py
@@ -15,13 +15,20 @@ import unittest
 # call_email_form_retriever() is the thing we are testing here
 # subclass the unittest.TestCase
 class KnownEMailValues(unittest.TestCase):
-    actual_output = ''
-    expected_output = ''
+    # A testcase is created by subclassing unittest.TestCase. The individual
+    # tests are defined with methods whose names start with the letters test.
+    # This naming convention informs the test runner about which methods represent tests.
+    def test_is_equal_values(self):
+        actual_output = ''
+        expected_output = ''
+        # self.assertEqual(actual_output, expected_output)
+        self.assertEqual("foo", "foo", "foo is not equal to foo")
 
 # to run a main program inside the modules, run like so:
 # python3 -m Tests.email_form_retriever_tests
+# with verbosity, python3 -m Tests.email_form_retriever_tests --verbose OR -v
 if __name__ == '__main__':
-    print("hello")
+    # print("hello")
     unittest.main()
 
 def start_test():
