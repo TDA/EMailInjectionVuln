229e459354f9eaeb650b73db4c187fc51c6c6627
Sai Pc <schand31@asu.edu>
2016-01-30 22:49:26 -0700

    making test case data

--- Tests/fuzzer_tests.py
+++ Tests/fuzzer_tests.py
@@ -8,7 +8,8 @@ __author__ = 'saipc'
 # action = 'MailTest.php'
 # action = 'http://localhost:63343/htdocs/TestProject/MailTest.php'
 
-# Input list: [{'name': 'pass', 'value': '', 'element_type': 'input', 'type': 'password'}, {'name': 'uname', 'value': '', 'element_type': 'input', 'type': 'text'}, {'name': 'email', 'value': '', 'element_type': 'input', 'type': 'email'}, {'name': '', 'value': 'Signup', 'element_type': 'input', 'type': 'submit'}]
-
+# Input list: [{'name': 'pass', 'value': '', 'element_type': 'input', 'type': 'password'}, {'name': 'uname', 'value': '', 'element_type': 'input', 'type': 'text'}, {'name': 'email', 'value': '', 'element_type': 'input', 'type': 'text'}, {'name': '', 'value': 'Signup', 'element_type': 'input', 'type': 'submit'}]
+# Input list: [{'name': 'pass', 'value': '', 'element_type': 'input', 'type': 'password'}, {'name': 'uname', 'value': '', 'element_type': 'input', 'type': 'text'}, {'name': 'mail', 'value': '', 'element_type': 'input', 'type': 'email'}, {'name': '', 'value': 'Signup', 'element_type': 'input', 'type': 'submit'}]
+# Input list: [{'name': 'pass', 'value': '', 'element_type': 'input', 'type': 'password'}, {'name': 'uname', 'value': '', 'element_type': 'input', 'type': 'text'}, {'name': 'mail', 'value': '', 'id': 'e-mail', 'element_type': 'input', 'type': 'text'}, {'name': '', 'value': 'Signup', 'element_type': 'input', 'type': 'submit'}]
 
 # this is the format : main_url, attributes, method, action, input_list = reconstructed_form
