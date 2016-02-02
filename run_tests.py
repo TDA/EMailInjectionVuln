be92130894f1fc0533b16c7e7f2f186835280968
Sai Pc <schand31@asu.edu>
2016-02-02 11:18:37 -0700

    Wrote a test runner

---
+++ run_tests.py
@@ -0,0 +1,20 @@
+__author__ = 'saipc'
+
+# python3 -m Tests.email_form_retriever_tests --verbose
+# python3 -m Tests.fuzzer_tests --verbose
+
+import os
+import subprocess
+
+testDir = "Tests"
+testDirContents=os.listdir("./" + testDir)
+flags = " --verbose"
+
+for file in testDirContents:
+    if (file.endswith("_tests.py")):
+        # run the test
+        fileWithoutExtension = file[:-3]
+        subprocess.call("python3 -m Tests." + fileWithoutExtension + flags, shell=True)
+        print(file)
+
+
