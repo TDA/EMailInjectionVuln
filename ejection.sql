157d6afd69c346a1bc79ebae31730a0746005818
Sai Pc <schand31@asu.edu>
2016-02-09 17:41:06 -0700

    edited ejection sql to bare minimum

--- ejection.sql
+++ ejection.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 15, 2015 at 05:46 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Feb 10, 2016 at 01:40 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ejection`
@@ -27,11 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `email_forms` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `form_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `form_id` (`form_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `id` int(10) NOT NULL,
+  `form_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `email_forms` (
 --
 
 CREATE TABLE IF NOT EXISTS `form` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `url` varchar(250) NOT NULL,
   `attributes` varchar(250) NOT NULL,
   `request_id` int(10) NOT NULL,
@@ -48,36 +46,117 @@ CREATE TABLE IF NOT EXISTS `form` (
   `method` varchar(5) NOT NULL,
   `action` varchar(250) NOT NULL,
   `absolute_action` varchar(250) NOT NULL,
-  `params` varchar(250) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `request_id` (`request_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+  `params` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fuzzed_forms`
+--
+
+CREATE TABLE IF NOT EXISTS `fuzzed_forms` (
+  `id` int(10) NOT NULL,
+  `form_id` int(10) NOT NULL,
+  `fuzz_string` varchar(256) NOT NULL,
+  `url_fuzzed` varchar(500) NOT NULL,
+  `payload_for_fuzzing` varchar(500) NOT NULL,
+  `time_of_fuzzing` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `params`
 --
 
 CREATE TABLE IF NOT EXISTS `params` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `element_type` varchar(20) NOT NULL,
   `type` varchar(20) NOT NULL,
   `name` varchar(50) NOT NULL,
   `value` varchar(50) NOT NULL,
-  `form_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=123 ;
+  `form_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `requests`
 --
 
 CREATE TABLE IF NOT EXISTS `requests` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `headers` varchar(1024) NOT NULL,
-  `url` varchar(250) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=69 ;
+  `url` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `email_forms`
+--
+ALTER TABLE `email_forms`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `form_id` (`form_id`);
+
+--
+-- Indexes for table `form`
+--
+ALTER TABLE `form`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `request_id` (`request_id`);
+
+--
+-- Indexes for table `fuzzed_forms`
+--
+ALTER TABLE `fuzzed_forms`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `form_id` (`form_id`);
+
+--
+-- Indexes for table `params`
+--
+ALTER TABLE `params`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `email_forms`
+--
+ALTER TABLE `email_forms`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `form`
+--
+ALTER TABLE `form`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fuzzed_forms`
+--
+ALTER TABLE `fuzzed_forms`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `params`
+--
+ALTER TABLE `params`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
