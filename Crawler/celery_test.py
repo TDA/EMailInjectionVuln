c5a6c27306bb6cb17b9d36e2c9a1d6cb7f7b829a
Sai Pc <schand31@asu.edu>
2016-02-13 17:01:41 -0700

    getting celery to run

--- Crawler/celery_test.py
+++ Crawler/celery_test.py
@@ -2,7 +2,10 @@ __author__ = 'saipc'
 
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://guest@localhost//')
+app = Celery('tasks')
+app.conf.update(
+    BROKER_URL = 'amqp://guest:guest@localhost:5678//'
+)
 
 @app.task(name = 'Crawler.celery_test.add')
 def add(x, y):
