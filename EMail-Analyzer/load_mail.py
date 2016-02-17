39dfa463dcc3267efa23febb49659c180e30122a
Sai Pc <schand31@asu.edu>
2016-02-17 00:57:40 -0700

    added docs

--- EMail-Analyzer/load_mail.py
+++ EMail-Analyzer/load_mail.py
@@ -4,8 +4,13 @@ import os
 mail_folder = '/var/mail'
 os.chdir(mail_folder)
 
-# the mails in maluser are direct proof of the
+# the mails in maluser are direct proof of the attack
 files = ['normaluser', 'maluser']
+# but the mails in normaluser could contain the x-check
+# header, if they do, then that is a successful attack
+# as well. This is due to pythons way of attaching
+# headers, instead of overwriting, it ignores duplicate
+# headers, so we need to inject a new one.
 NO_INJECTION_FILE = 'reguser'
 
 normal_mails = []
