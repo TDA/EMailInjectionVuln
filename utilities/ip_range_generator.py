3e1b0804d14fc9914dcf25ac1fe2f40f1b102b71
Sai Pc <schand31@asu.edu>
2016-03-07 12:27:05 -0700

    updated utilities

--- utilities/ip_range_generator.py
+++ utilities/ip_range_generator.py
@@ -2,10 +2,17 @@
 
 __author__ = "Sai Pc"
 
+netmask  = "255.255.255.192"
+gateway = "129.219.234.193"
+dns_nameservers = "129.219.17.200"
+base_addr_first_three_segs = "129.219.234."
+start_ip = 195
+
+# set up for 58 ips in total, mapping
+# them to the same nic
 for i in xrange(2, 60):
-	print """iface eth0:""" + str(i) + """ inet static
-	address 129.219.234.""" + str(195 + i) + """ 
-	netmask 255.255.255.192
-	gateway 129.219.234.193
-	dns-nameservers 129.219.17.200
-"""
+	print "iface eth0:" + str(i) + """ inet static
+	address """ + base_addr_first_three_segs + str(start_ip + i) + """
+	netmask """ + netmask + """
+	gateway """ + gateway + """
+	dns-nameservers """ + dns_nameservers
