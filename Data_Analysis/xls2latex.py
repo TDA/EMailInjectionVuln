63b860f2e4813798a8ba01faf87d62e08cc17696
Sai Pc <schand31@asu.edu>
2016-03-25 00:06:54 -0700

    xls2latex done wooohoooo

---
+++ Data_Analysis/xls2latex.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import sys
+import csv
+
+__author__ = 'saipc'
+
+def xls2latex(filename):
+    with open(filename, 'r') as file:
+        contents = csv.reader(file)
+        new_rows = []
+        for row in contents:
+            new_rows.append(' & '.join(row))
+        latex_table = ' \\\\\n\hline\n'.join(new_rows)
+        print(latex_table)
+
+if __name__ == '__main__':
+    filename = None
+    if (len(sys.argv) < 2):
+        print("Usage: python xls2latex filename")
+        print("Exiting")
+        filename = r"~/Documents/schand31_thesis/Results/pipeline.csv"
+    filename = filename or sys.argv[1]
+    print(filename)
+    xls2latex(filename)
