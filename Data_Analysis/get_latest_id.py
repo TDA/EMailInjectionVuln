d77196384932bc5ef1d401151dec5dd43e13b306
Sai Pc <schand31@asu.edu>
2016-03-26 11:09:15 -0700

    cleaning up

--- Data_Analysis/get_latest_id.py
+++ Data_Analysis/get_latest_id.py
@@ -3,6 +3,7 @@ __author__ = 'saipc'
 
 from functions import *
 import time
+import subprocess
 
 def get_latest_id(form):
     query = "SELECT `id` FROM `%s` GROUP by id DESC LIMIT 1"%(form)
@@ -17,6 +18,7 @@ def construct_dump_query(form):
     id = get_latest_id(form)
     dump_query = "/opt/lampp/bin/mysqldump --user=root -p --host=localhost ejection %s --where='id>%s' > ~/%s_dump_%s.sql"%(form, id, form, time.strftime("%b_%d").lower())
     print(dump_query)
+    # subprocess.call(dump_query, shell=True)
 
 if __name__ == '__main__':
     construct_dump_query('form')
