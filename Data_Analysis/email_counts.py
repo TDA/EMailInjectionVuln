bcdd5a1c4b8bee84a08f5e97853ec0fadc33647e
Sai Pc <schand31@asu.edu>
2016-04-10 22:51:17 -0700

    remodeled email_counts to make it easy to c/p

--- Data_Analysis/email_counts.py
+++ Data_Analysis/email_counts.py
@@ -13,12 +13,13 @@ def email_counts(form_name, field):
     print("Ehi Alone", len(form_ids))
     ehi_alone_count = len(form_ids)
 
-    # EHI and x-check -- 137
-    ehi_and_x_check = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `recipient_email` NOT LIKE '%bcc%' AND `x-check` = 1 AND `recipient_email` LIKE '%mal%' LIMIT 0, 500"
-    cursor.execute(ehi_and_x_check)
+
+    # x-check alone -- 153 (137 with mal, 16 with nuser)
+    x_check = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `x-check` = 1"
+    cursor.execute(x_check)
     form_ids = cursor.fetchall()
-    print("Ehi and x check", len(form_ids))
-    ehi_and_x_check_ids = form_ids
+    print("x-check alone", len(form_ids))
+
 
     # To inj Alone - 83
     to_inj = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `to_injection` = 1"
@@ -27,17 +28,19 @@ def email_counts(form_name, field):
     print("To inj Alone", len(form_ids))
     to_inj_count = len(form_ids)
 
+
+    # EHI and x-check -- 137
+    ehi_and_x_check = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `recipient_email` NOT LIKE '%bcc%' AND `x-check` = 1 AND `recipient_email` LIKE '%mal%' LIMIT 0, 500"
+    cursor.execute(ehi_and_x_check)
+    form_ids = cursor.fetchall()
+    print("Ehi and x check", len(form_ids))
+    ehi_and_x_check_ids = form_ids
+
     #To inj AND x-check -- 8
     to_inj_and_x_check = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `to_injection` = 1 AND `x-check` = 1 LIMIT 0, 1000"
     cursor.execute(to_inj_and_x_check)
     form_ids = cursor.fetchall()
     print("To inj AND x-check", len(form_ids))
-
-    # x-check alone -- 153 (137 with mal, 16 with nuser)
-    x_check = "SELECT DISTINCT `form_id` FROM `successful_attack_emails` WHERE `x-check` = 1"
-    cursor.execute(x_check)
-    form_ids = cursor.fetchall()
-    print("x-check alone", len(form_ids))
 
 
     # x-check AND nuser -- 40 (16 unique ones apart from mal)
