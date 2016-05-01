bc3f5e7c825b77006e3b5fb8d93616c9c2635588
Sai Pc <schand31@asu.edu>
2016-05-01 02:36:59 -0700

    updating resend emails

--- EMail-Generator/resend_emails.py
+++ EMail-Generator/resend_emails.py
@@ -1,12 +1,23 @@
 __author__ = 'saipc'
+import whois
 
 def populate_domains(fname):
     domains = set()
     with open(fname, 'r') as f:
         for line in f:
             domains.add(str(line).split(" --- ")[0])
-    print(domains)
-    print(len(domains))
+    return domains
 
 if __name__ == '__main__':
-    populate_domains("failed_emails")
+    domains = populate_domains("failed_emails")
+    mailed_domains = 0
+    for domain in domains:
+        try:
+            whois_info = whois.whois(domain)
+            # print(whois_info)
+            print(domain)
+            if "emails" in whois_info and (whois_info["emails"] != None):
+                emails = whois_info["emails"]
+                mailed_domains += 1
+        except Exception as e:
+            print("Skipping " + domain)
