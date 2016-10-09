a4297bc15fb623065305c264c77957a99b73a1f2
Sai Chandramouli <schand31@asu.edu>
2016-10-09 00:58:38 -0700

    almost done with virus parsing, need to write o/p to file

--- Virus_Analysis/utils.py
+++ Virus_Analysis/utils.py
@@ -7,8 +7,12 @@ def load_json_file(file):
         json_data = json.loads(json_file.read())
         return json_data
 
-def parse_json(jsonData):
+def save_json_file(file, json_data):
+    with open(file, 'w') as json_file:
+        json_file.write(json.dumps(json_data))
+
+def parse_json(json_data):
     pass
 
-def get_size_json(jsonData):
-    return len(jsonData.keys())
+def get_size_json(json_data):
+    return len(json_data.keys())
--- Virus_Analysis/virus_results.py
+++ Virus_Analysis/virus_results.py
@@ -2,6 +2,15 @@ from utils import *
 
 __author__ = 'saipc'
 
+def find_positives(jsonData):
+    positive_hits = []
+    for entry_key in jsonData.keys():
+        print("Searching key", entry_key)
+        if (jsonData[entry_key]["positives"]) > 0:
+            print("Found a positive", entry_key)
+            positive_hits.append(jsonData[entry_key])
+    return positive_hits
+
 if __name__ == '__main__':
-    jsonData = load_json_file('Data/vt_results.json')
-    print(get_size_json(jsonData))
+    json_data = load_json_file('Data/vt_results.json')
+    positives = find_positives(json_data)
