7d21d84b6fcf2a17f7727dc2ad29da3f6ee1eb30
Sai Chandramouli <schand31@asu.edu>
2017-02-07 19:36:59 +0530

    interfaced with alexa

--- Data_Analysis/alexa_website_data.py
+++ Data_Analysis/alexa_website_data.py
@@ -1,10 +1,17 @@
 __author__ = 'saipc'
 
 from total_unique_domains import get_domains_from_list
+import requests
+
+def get_alexa_data(url):
+  constructed_url = 'http://data.alexa.com/data?cli=10&url=' + url
+  r = requests.get(constructed_url)
+  print(r.text)
+
 
 if __name__ == '__main__':
   with open('domains_list', 'r') as domains_file:
     domains = [domain.strip() for domain in domains_file]
     unique_domains = get_domains_from_list(domains)
     for domain in unique_domains:
-      print(domain)
+      get_alexa_data(domain)
