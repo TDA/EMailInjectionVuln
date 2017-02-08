c140159e42cd93a75ea363152e99a15f36e6b060
Sai Chandramouli <schand31@asu.edu>
2017-02-08 19:46:23 +0530

    falling into ruby bad habits added xml parsing

--- Data_Analysis/alexa_website_data.py
+++ Data_Analysis/alexa_website_data.py
@@ -2,16 +2,25 @@ __author__ = 'saipc'
 
 from total_unique_domains import get_domains_from_list
 import requests
+import xml.etree.ElementTree as ET
+
 
 def get_alexa_data(url):
   constructed_url = 'http://data.alexa.com/data?cli=10&url=' + url
   r = requests.get(constructed_url)
-  print(r.text)
+  parsed_xml = get_xml(r.text)
+  get_ranks(parsed_xml)
 
+def get_xml(xml_string):
+  return ET.fromstring(xml_string)
+
+def get_ranks(xml_data):
+  print(xml_data.tag)
+  print(xml_data.attrib)
 
 if __name__ == '__main__':
   with open('domains_list', 'r') as domains_file:
     domains = [domain.strip() for domain in domains_file]
     unique_domains = get_domains_from_list(domains)
-    for domain in unique_domains:
-      get_alexa_data(domain)
+    # for domain in unique_domains:
+    get_alexa_data(unique_domains.pop())
