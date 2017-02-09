6d39a763a95ce2190f7205928d5a414754450628
Sai Chandramouli <schand31@asu.edu>
2017-02-09 20:23:32 +0530

    finished gathering data, need to represent it in the right way

--- Data_Analysis/alexa_website_data.py
+++ Data_Analysis/alexa_website_data.py
@@ -7,20 +7,29 @@ import xml.etree.ElementTree as ET
 
 def get_alexa_data(url):
   constructed_url = 'http://data.alexa.com/data?cli=10&url=' + url
+  print('querying', constructed_url)
   r = requests.get(constructed_url)
   parsed_xml = get_xml(r.text)
   get_ranks(parsed_xml)
 
 def get_xml(xml_string):
+  print('obtained XML')
   return ET.fromstring(xml_string)
 
 def get_ranks(xml_data):
-  print(xml_data.tag)
-  print(xml_data.attrib)
+  print('getting rank')
+  interested_attrs = ['POPULARITY', 'COUNTRY']
+  # data is always inside the first tag, if present
+  if (len(xml_data) > 0):
+    sd_data = xml_data[0]
+    for child in sd_data:
+      # we are interested only in the popularity and country stats
+      if child.tag in interested_attrs:
+        print(child.tag, child.attrib)
 
 if __name__ == '__main__':
   with open('domains_list', 'r') as domains_file:
     domains = [domain.strip() for domain in domains_file]
     unique_domains = get_domains_from_list(domains)
-    # for domain in unique_domains:
-    get_alexa_data(unique_domains.pop())
+    for domain in unique_domains:
+      get_alexa_data(domain)
