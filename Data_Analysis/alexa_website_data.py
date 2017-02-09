92cdf5305e72c9f209da2d2e3b9e9f82776220fb
Sai Chandramouli <schand31@asu.edu>
2017-02-09 21:14:16 +0530

    finishing up the list of ranks

--- Data_Analysis/alexa_website_data.py
+++ Data_Analysis/alexa_website_data.py
@@ -10,22 +10,35 @@ def get_alexa_data(url):
   print('querying', constructed_url)
   r = requests.get(constructed_url)
   parsed_xml = get_xml(r.text)
-  get_ranks(parsed_xml)
+  ranks_list = []
+  ranks_list.append(get_ranks(parsed_xml))
+  print(len(ranks_list))
 
 def get_xml(xml_string):
-  print('obtained XML')
+  # print('obtained XML')
   return ET.fromstring(xml_string)
 
 def get_ranks(xml_data):
-  print('getting rank')
-  interested_attrs = ['POPULARITY', 'COUNTRY']
+  # print('getting rank')
   # data is always inside the first tag, if present
   if (len(xml_data) > 0):
     sd_data = xml_data[0]
+    # we are interested only in the popularity and country stats
+    url =''
+    world_pop = ''
+    country_name = ''
+    country_rank = ''
     for child in sd_data:
-      # we are interested only in the popularity and country stats
-      if child.tag in interested_attrs:
-        print(child.tag, child.attrib)
+      child_tag_attr = child.attrib
+      if child.tag == 'POPULARITY':
+        url = child_tag_attr['URL']
+        world_pop = child_tag_attr['TEXT']
+      elif child.tag == 'COUNTRY':
+        country_name = child_tag_attr['NAME']
+        country_rank = child_tag_attr['RANK']
+    print(url, world_pop, country_name, country_rank)
+    ranks = (url, world_pop, country_name, country_rank)
+  return ranks
 
 if __name__ == '__main__':
   with open('domains_list', 'r') as domains_file:
