aea2a089737d4527ac5fb87fa264d25e2f33ad9d
Sai Chandramouli <schand31@asu.edu>
2017-02-09 22:37:02 +0530

    updating lsits

--- Data_Analysis/alexa_website_data.py
+++ Data_Analysis/alexa_website_data.py
@@ -10,13 +10,15 @@ def get_alexa_data(url):
   print('querying', constructed_url)
   r = requests.get(constructed_url)
   parsed_xml = get_xml(r.text)
-  ranks_list = []
-  ranks_list.append(get_ranks(parsed_xml))
-  print(len(ranks_list))
+  return get_ranks(parsed_xml)
 
 def get_xml(xml_string):
   # print('obtained XML')
-  return ET.fromstring(xml_string)
+  try:
+    return ET.fromstring(xml_string)
+  except Exception as e:
+    print("Was not able to parse this one", xml_string)
+    return []
 
 def get_ranks(xml_data):
   # print('getting rank')
@@ -37,12 +39,16 @@ def get_ranks(xml_data):
         country_name = child_tag_attr['NAME']
         country_rank = child_tag_attr['RANK']
     print(url, world_pop, country_name, country_rank)
-    ranks = (url, world_pop, country_name, country_rank)
-  return ranks
+    return (url, world_pop, country_name, country_rank)
 
 if __name__ == '__main__':
   with open('domains_list', 'r') as domains_file:
     domains = [domain.strip() for domain in domains_file]
     unique_domains = get_domains_from_list(domains)
+    ranks_list = []
     for domain in unique_domains:
-      get_alexa_data(domain)
+      ranks_list.append(get_alexa_data(domain))
+    ranks_list = filter(None, ranks_list)
+    print(len(ranks_list))
+    with open('output_alexa_domains_list', 'w') as op_file:
+      op_file.write(",\n".join(ranks_list))
